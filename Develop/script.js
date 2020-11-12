d2b5b66a06e3a2a7469f65ff0f955a1089b9d832
rpc08002 <rcurtin0491@gmail.com>
2020-11-12 17:01:08 -0500

    added space in variables

--- Develop/script.js
+++ Develop/script.js
@@ -11,8 +11,9 @@ character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "'
 // Numeric Characters
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Alphabetical Characters
-alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Space for uppercase conversion
+space = [];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
