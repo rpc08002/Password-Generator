4083be913b7b79d9fb874d827b4e374f88887475
rpc08002 <rcurtin0491@gmail.com>
2020-11-12 18:27:00 -0500

    fixed indent on line 17

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,7 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Space for uppercase conversion
 space = [];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
