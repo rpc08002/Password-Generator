98b49888892b8581ba8016c01d2295e9880be19d
rpc08002 <rcurtin0491@gmail.com>
2020-11-12 16:51:35 -0500

    listed out variable values

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,14 @@ var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
+// Password variable values
+// Special Characters
+character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "'", ",", "-", ".", "/", "[", "]", "`", "~", "{", "}", ";", ":", "|", "?", "<", ">"];
+// Numeric Characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Alphabetical Characters
+alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
