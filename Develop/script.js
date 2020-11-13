a82d54f39bf33613efee3028fe049bb02b1e439e
rpc08002 <rcurtin0491@gmail.com>
2020-11-13 16:32:45 -0500

    added variable for uppercase/variable choices

--- Develop/script.js
+++ Develop/script.js
@@ -14,9 +14,16 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Space for uppercase conversion
 space = [];
+// Choices declared outside the if statement so they can be connected after condition
+var choices;
+var toUpper = function (x) {
+  return x.toUpper();
+};
+// creates a variable for uppercase
+aplha2 = alpha.map(toUpper);
+//Assignment Code
+var get = document.querySelector("#generate");
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
